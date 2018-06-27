6d79bb171283200f5ee1c88bbdeaf92825afae43
ipekyalki <ipekyalki@gmail.com>
2018-06-27 09:24:31 +0300

    week2 script updated

--- week2.R
+++ week2.R
@@ -2,7 +2,8 @@
 
 control <- filter(femaleMiceWeights, Diet == "chow") %>% select(Bodyweight) %>% unlist
 treatment <- filter(femaleMiceWeights, Diet == "hf") %>% select(Bodyweight) %>% unlist
-mean(treatment) - mean(control)
+obs <- mean(treatment) - mean(control)
+
 
 #Download the femaleControlsPopulation file
 library(downloader) 
@@ -33,3 +34,65 @@ abs(mean(sample(femaleControlsPopulation,5) - mean(femaleControlsPopulation)))
 set.seed(5)
 abs(mean(sample(femaleControlsPopulation,5) - mean(femaleControlsPopulation)))
 
+#Week 2 Null Distribution Notes :
+
+control <- filter(femaleMiceWeights, Diet == "chow") %>% select(Bodyweight) %>% unlist
+treatment <- filter(femaleMiceWeights, Diet == "hf") %>% select(Bodyweight) %>% unlist
+obs <- mean(treatment) - mean(control)
+
+View(femaleControlsPopulation)
+femaleControlsPopulation <- unlist(femaleControlsPopulation)
+femaleControlsPopulation
+
+n <- 10000
+nulls <- vector("numeric",n)
+for(i in 1:n){
+control2 <- sample(femaleControlsPopulation, 12)
+treatment2 <- sample(femaleControlsPopulation, 12)
+nulls[i] <- mean(treatment2) - mean(control2)
+}
+
+max(nulls)
+hist(nulls)
+
+sum(nulls > obs) / n #p-value
+#OR
+mean(abs(nulls) > obs) #p-value
+
+#Null Distribution Exercises
+
+#Q1:Set the seed at 1, then using a for-loop take a random sample of 5 mice 1,000 times. Save these averages. 
+#What proportion of these 1,000 averages are more than 1 gram away from the average of x ?
+avg_data <- mean(femaleControlsPopulation)
+head(femaleControlsPopulation)
+set.seed(3)
+n <- 1000
+averages <- vector("numeric",n)
+for(i in 1:n){
+  X <- sample(femaleControlsPopulation, 5)
+  averages[i] <- mean(X)
+}
+mean((abs(averages - avg_data)) > 1)
+
+#Q2: We are now going to increase the number of times we redo the sample from 1,000 to 10,000. 
+#Set the seed at 1, then using a for-loop take a random sample of 5 mice 10,000 times. Save these averages. 
+#What proportion of these 10,000 averages are more than 1 gram away from the average of x ?
+set.seed(3)
+n <- 10000
+averages <- vector("numeric",n)
+for(i in 1:n){
+  X <- sample(femaleControlsPopulation, 5)
+  averages[i] <- mean(X)
+} 
+mean((abs(averages - avg_data)) > 1)
+
+#Set the seed at 1, then using a for-loop take a random sample of 50 mice 1,000 times. Save these averages. 
+#What proportion of these 1,000 averages are more than 1 gram away from the average of x ?
+set.seed(3)
+n <- 1000
+averages2 <- vector("numeric",n)
+for(i in 1:n){
+  X <- sample(femaleControlsPopulation, 50)
+  averages2[i] <- mean(X)
+} 
+mean((abs(averages2 - avg_data)) > 1)
